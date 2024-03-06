4ba80e50e3a0ff338b119a24dfd05fb4e34894b5
Paulo Vargas <paulotomegomesdevargas@hotmail.com>
2024-03-06 14:29:19 -0300

    Inicial

---
+++ inverte_string.js
@@ -0,0 +1,20 @@
+/* 5) Escreva um programa que inverta os caracteres de um string.
+
+
+IMPORTANTE:
+
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+
+b) Evite usar funções prontas, como, por exemplo, reverse; */
+
+function inverterString(str) {
+    let inverted = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+      inverted += str[i];
+    }
+    return inverted;
+  }
+  
+  const minhaString = 'Olá, quero ser estagiário da Target! ❤️'; // String para inverter
+  console.log(inverterString(minhaString)); // Saída: '!dlroW ,olleH'
+  
